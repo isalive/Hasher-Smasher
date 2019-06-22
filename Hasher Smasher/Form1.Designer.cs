76295a8956158ce6e30c9882cd07ed7360a08fb4
isalive <isalive@protonmail.com>
2019-06-22 15:47:59 +0200

    Add project files.

---
+++ Hasher Smasher/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Hasher_Smasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbkeyinput = new System.Windows.Forms.TextBox();
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.btencrypt = new System.Windows.Forms.Button();
+            this.btdecrypt = new System.Windows.Forms.Button();
+            this.tboutput = new System.Windows.Forms.TextBox();
+            this.btclearall = new System.Windows.Forms.Button();
+            this.btcopyinput = new System.Windows.Forms.Button();
+            this.btcopyoutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbkeyinput
+            // 
+            this.tbkeyinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbkeyinput.Location = new System.Drawing.Point(94, 12);
+            this.tbkeyinput.Name = "tbkeyinput";
+            this.tbkeyinput.Size = new System.Drawing.Size(214, 31);
+            this.tbkeyinput.TabIndex = 0;
+            this.tbkeyinput.UseSystemPasswordChar = true;
+            // 
+            // tbinput
+            // 
+            this.tbinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbinput.Location = new System.Drawing.Point(12, 49);
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(356, 31);
+            this.tbinput.TabIndex = 1;
+            this.tbinput.Text = "Insert String to Encrypt";
+            this.tbinput.Click += new System.EventHandler(this.Tbinput_Click);
+            this.tbinput.Leave += new System.EventHandler(this.Tbinput_Leave);
+            // 
+            // btencrypt
+            // 
+            this.btencrypt.Location = new System.Drawing.Point(12, 127);
+            this.btencrypt.Name = "btencrypt";
+            this.btencrypt.Size = new System.Drawing.Size(139, 56);
+            this.btencrypt.TabIndex = 2;
+            this.btencrypt.Text = "Check-Encrypt";
+            this.btencrypt.UseVisualStyleBackColor = true;
+            this.btencrypt.Click += new System.EventHandler(this.Btencrypt_Click);
+            // 
+            // btdecrypt
+            // 
+            this.btdecrypt.Location = new System.Drawing.Point(266, 127);
+            this.btdecrypt.Name = "btdecrypt";
+            this.btdecrypt.Size = new System.Drawing.Size(139, 56);
+            this.btdecrypt.TabIndex = 3;
+            this.btdecrypt.Text = "Decrypt";
+            this.btdecrypt.UseVisualStyleBackColor = true;
+            this.btdecrypt.Click += new System.EventHandler(this.Btdecrypt_Click);
+            // 
+            // tboutput
+            // 
+            this.tboutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboutput.Location = new System.Drawing.Point(12, 86);
+            this.tboutput.Name = "tboutput";
+            this.tboutput.Size = new System.Drawing.Size(356, 31);
+            this.tboutput.TabIndex = 4;
+            this.tboutput.Text = "Insert String to Decrypt";
+            this.tboutput.Click += new System.EventHandler(this.Tboutput_Click);
+            this.tboutput.Leave += new System.EventHandler(this.Tboutput_Leave);
+            // 
+            // btclearall
+            // 
+            this.btclearall.Location = new System.Drawing.Point(157, 127);
+            this.btclearall.Name = "btclearall";
+            this.btclearall.Size = new System.Drawing.Size(103, 56);
+            this.btclearall.TabIndex = 5;
+            this.btclearall.Text = "Clear All";
+            this.btclearall.UseVisualStyleBackColor = true;
+            this.btclearall.Click += new System.EventHandler(this.Btclearall_Click);
+            // 
+            // btcopyinput
+            // 
+            this.btcopyinput.Location = new System.Drawing.Point(371, 49);
+            this.btcopyinput.Name = "btcopyinput";
+            this.btcopyinput.Size = new System.Drawing.Size(40, 31);
+            this.btcopyinput.TabIndex = 6;
+            this.btcopyinput.Text = "C";
+            this.btcopyinput.UseVisualStyleBackColor = true;
+            this.btcopyinput.Click += new System.EventHandler(this.Btcopyinput_Click);
+            // 
+            // btcopyoutput
+            // 
+            this.btcopyoutput.Location = new System.Drawing.Point(371, 86);
+            this.btcopyoutput.Name = "btcopyoutput";
+            this.btcopyoutput.Size = new System.Drawing.Size(40, 31);
+            this.btcopyoutput.TabIndex = 7;
+            this.btcopyoutput.Text = "C";
+            this.btcopyoutput.UseVisualStyleBackColor = true;
+            this.btcopyoutput.Click += new System.EventHandler(this.Btcopyoutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 195);
+            this.Controls.Add(this.btcopyoutput);
+            this.Controls.Add(this.btcopyinput);
+            this.Controls.Add(this.btclearall);
+            this.Controls.Add(this.tboutput);
+            this.Controls.Add(this.btdecrypt);
+            this.Controls.Add(this.btencrypt);
+            this.Controls.Add(this.tbinput);
+            this.Controls.Add(this.tbkeyinput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HasherSmasher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbkeyinput;
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.Button btencrypt;
+        private System.Windows.Forms.Button btdecrypt;
+        private System.Windows.Forms.TextBox tboutput;
+        private System.Windows.Forms.Button btclearall;
+        private System.Windows.Forms.Button btcopyinput;
+        private System.Windows.Forms.Button btcopyoutput;
+    }
+}
+
---
+++ Hasher Smasher/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hasher_Smasher
+{
+    public partial class Form1 : Form
+    {
+        private SecretCode mycode;
+
+        private string inputkey;
+        private string stringtoencrypt;
+        private string stringtodecrypt;
+        public Form1()
+        {
+            InitializeComponent();
+            mycode = new SecretCode();
+            tbkeyinput.Text = "";
+        }
+
+        public bool Comparepassword()
+        {
+            if (!(string.IsNullOrEmpty(tbkeyinput.Text)))
+                inputkey = tbkeyinput.Text;
+            else return false;
+
+            if (mycode.Compare(inputkey) == false)
+            {
+                MessageBox.Show("Secret Key is incorrect");
+                return false;
+            }
+            else return true;
+        }
+
+        private void Btencrypt_Click(object sender, EventArgs e)
+        {
+            if (Comparepassword() && !(string.IsNullOrEmpty(tbinput.Text)))
+            {
+                stringtoencrypt = tbinput.Text;
+                tboutput.Text = mycode.Encrypt(stringtoencrypt, inputkey);
+            }
+            else
+                MessageBox.Show("Password / Input String cannot be empty or wrong");
+
+            //else
+            //    MessageBox.Show("Secret Key empty, please input the secret key");
+
+            //else if ())
+            //    stringtoencrypt = tbinput.Text;
+
+            //else if (string.IsNullOrEmpty(tbinput.Text))
+            //    MessageBox.Show("Input box is empty");
+
+            //if (!(string.IsNullOrEmpty(tbkeyinput.Text))) 
+        }
+
+        private void Btclearall_Click(object sender, EventArgs e)
+        {
+            tbkeyinput.Text = "";
+            tbinput.Text = "Decrypt / Input";
+            tboutput.Text = "Encrypt / Output";
+        }
+
+        private void Btdecrypt_Click(object sender, EventArgs e)
+        {
+            if (Comparepassword() && !(string.IsNullOrEmpty(tbinput.Text)))
+            {
+                stringtodecrypt = tboutput.Text;
+                tbinput.Text = mycode.Decrypt(stringtodecrypt, inputkey);
+            }
+
+            //if (!(string.IsNullOrEmpty(tbkeyinput.Text)))
+            //    inputkey = tbkeyinput.Text;
+
+                //else
+                //    MessageBox.Show("Secret Key empty, please input the secret key");
+
+                //if (mycode.Compare(inputkey) == false)
+                //    MessageBox.Show("Secret Key different from database");
+
+                //if (!(string.IsNullOrEmpty(tboutput.Text)))
+                //    stringtoencrypt = tboutput.Text;
+
+                //else
+                //    MessageBox.Show("Output box is empty");
+
+                //if (!(string.IsNullOrEmpty(tbkeyinput.Text)))
+                //    tbinput.Text = mycode.Decrypt(stringtoencrypt, inputkey);
+        }
+
+        private void Btcopyinput_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbinput.Text);
+        }
+
+        private void Btcopyoutput_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tboutput.Text);
+        }
+
+        private void Tbinput_Click(object sender, EventArgs e)
+        {
+            if (tbinput.Text == "Insert String to Encrypt")
+                tbinput.Text = "";
+        }
+
+        private void Tboutput_Click(object sender, EventArgs e)
+        {
+            if (tboutput.Text == "Insert String to Decrypt")
+                tboutput.Text = "";
+        }
+
+        private void Tbinput_Leave(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(tbinput.Text))
+                tbinput.Text = "Insert String to Encrypt";
+        }
+
+        private void Tboutput_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tboutput.Text))
+                tboutput.Text = "Insert String to Decrypt";
+        }
+    }
+}
